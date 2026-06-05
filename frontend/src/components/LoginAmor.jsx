79d7a08841f06f8ff273ba910085856c6566ca69
jeivizz <jvictorsilva65@gmail.com>
2026-06-04 21:21:26 -0300

    adjusting background

--- frontend/src/components/LoginAmor.jsx
+++ frontend/src/components/LoginAmor.jsx
@@ -12,7 +12,7 @@ const LoginAmor = ({ onConfirm }) => {
     };
 
     return (
-        <div className="fixed inset-0  flex items-center justify-center z-50 p-4"
+        <div className="fixed w-full h-full flex items-center justify-center z-50 p-4"
                 style={{backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='1.5' numOctaves='8'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23n)' opacity='0.20'/%3E%3C/svg%3E"),
                                           linear-gradient(to bottom, #eb3434, #ff8585)`,}}
         >
